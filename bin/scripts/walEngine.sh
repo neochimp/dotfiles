2559968f616eaad792b32ae90f663ee4fda95698
Cameron Lee <7cameronlee@gmail.com>
2025-10-29 18:32:41 -0700

    updated wallpaper script to be more robust and added -h and -l flags for listing available photos

--- bin/scripts/walEngine.sh
+++ bin/scripts/walEngine.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
-FILE="/home/soup/.config/wallpapers/$1"
+DIR="/home/soup/.config/wallpapers"
 
+Help() {
+  echo "This script is used to change the wallaper and themes"
+  echo
+  echo "Syntax: wallpaper [name of file]"
+  echo "Options:"
+  echo "h   Print this help message."
+  echo "l   List out available files in wallpaper directory"
+}
+
+while getopts "hl" opt; do
+  case $opt in
+  h) # Display Help
+    Help
+    exit 0
+    ;;
+  l)
+    echo "Available files"
+    ls "$DIR"*.jpg "DIR"*.png /home/soup/.config/wallpapers/ 2>/dev/null
+    exit 0
+    ;;
+  esac
+done
+
+FILE="$DIR/$1"
 if [ -f "$FILE" ]; then
   printf "preload = $FILE\nwallpaper = ,$FILE" >.config/hypr/hyprpaper.conf
   WAL=$(/bin/wal -i "$FILE")
